32b6e30ce1c52c98f781959214f70cc8b8916659
Fred McWilliams <fredmcwilliams92@gmail.com>
2021-05-01 20:38:22 -0500

    added in fetch and function to search a city and retreive certain information from teh api

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+// global variables
+var userInput;
+var city;
+var temp;
+var wind;
+var humidity;
+var uvIndex;
+var latitude;
+var longitude;
+
+// search button event listener
+var searchButton = document.getElementById('searchBtn');
+searchButton.addEventListener('click', cityInput);
+
+// api key
+const apiKey = '4d627c952a573f026a800ae6494594c8'
+
+// get the city input
+function cityInput() {
+userInput = document.getElementById('userInput').value;
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiKey)
+.then(function(response) {
+  return response.json();
+})
+.then(function(data) {
+  console.log(data);
+
+  latitude = data.coord.lat;
+  longitude = data.coord.lon;
+
+  kelvinToFarenheit = (Math.round(data.main.temp - 273.15) * 9 / 5 + 32);
+  temp = document.querySelector('#temp').innerHTML = 'Temp: ' + kelvinToFarenheit;
+
+  wind = document.querySelector('#wind').innerHTML = 'Wind: ' + data.wind.speed;
+  humidity = document.querySelector('#humidity').innerHTML = 'Humidity: ' + data.main.humidity;
+
+  
+  });
+}
